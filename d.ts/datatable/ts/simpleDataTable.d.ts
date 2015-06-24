2ef786f73e19c3dc6b7f7b9f4a45dc01be2c1b9e
Stan Lewis <slewis@fusesource.com>
2015-06-24 14:34:02 -0400

    Rework the directive so it's not a class

--- d.ts/datatable/ts/simpleDataTable.d.ts
+++ d.ts/datatable/ts/simpleDataTable.d.ts
@@ -3,16 +3,4 @@
  * @module DataTable
  */
 declare module DataTable {
-    class SimpleDataTable {
-        $compile: any;
-        restrict: string;
-        scope: {
-            config: string;
-            target: string;
-            showFiles: string;
-        };
-        link: (scope, element, attrs) => any;
-        constructor($compile: any);
-        private doLink($scope, $element, $attrs);
-    }
 }
