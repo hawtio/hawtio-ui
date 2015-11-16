acebb18fb234667aafb057da2b8bfc6ef86d9d10
Stan Lewis <slewis@fusesource.com>
2015-11-16 15:21:41 -0500

    Calculate the height stuff should be instead

--- plugins/ui/ts/windowHeight.ts
+++ plugins/ui/ts/windowHeight.ts
@@ -2,20 +2,39 @@
 //
 module UI {
 
-  var log = Logger.get('hawtio-window-height');
-
   _module.directive('hawtioWindowHeight', ['$window', ($window) => {
     return {
       restrict: 'A',
       replace: false,
       link: (scope, element, attrs) => {
   
-        var viewportHeight = $window.height;
-        log.debug("Viewport height: ", viewportHeight);
+        var viewportHeight = $window.innerHeight;
+        console.log("Viewport height: ", viewportHeight);
 
-        element.parents().attr({
-          'style': 'height: 100vh'
-        });
+        function processElement(el) {
+          var offset = el.offset();
+          if (!offset) {
+            return;
+          }
+          var top = offset.top;
+          var height = viewportHeight - top;
+          if (height > 0) {
+            el.attr({
+              'style': 'height: ' + height + 'px;'
+            });
+          }
+        }
+
+        function layout() {
+          element.parents().each((index, el) => {
+            el = $(el);
+            processElement(el);
+          });
+          processElement(element);
+        }
+        //layout();
+        scope.$watch(_.debounce(layout, 1000, { trailing: true}));
+        //$($window).on('resize', layout);
       }
     };
   }]);
