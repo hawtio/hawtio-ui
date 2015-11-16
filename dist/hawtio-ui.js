c6d3a4151c7b44bdb0aa6a54c3ca827f7c924f25
circleci <circleci@mail.com>
2015-11-16 20:23:50 +0000

    [ci skip] Update site

--- dist/hawtio-ui.js
+++ dist/hawtio-ui.js
@@ -3918,17 +3918,36 @@ var UI;
 //
 var UI;
 (function (UI) {
-    var log = Logger.get('hawtio-window-height');
     UI._module.directive('hawtioWindowHeight', ['$window', function ($window) {
             return {
                 restrict: 'A',
                 replace: false,
                 link: function (scope, element, attrs) {
-                    var viewportHeight = $window.height;
-                    log.debug("Viewport height: ", viewportHeight);
-                    element.parents().attr({
-                        'style': 'height: 100vh'
-                    });
+                    var viewportHeight = $window.innerHeight;
+                    console.log("Viewport height: ", viewportHeight);
+                    function processElement(el) {
+                        var offset = el.offset();
+                        if (!offset) {
+                            return;
+                        }
+                        var top = offset.top;
+                        var height = viewportHeight - top;
+                        if (height > 0) {
+                            el.attr({
+                                'style': 'height: ' + height + 'px;'
+                            });
+                        }
+                    }
+                    function layout() {
+                        element.parents().each(function (index, el) {
+                            el = $(el);
+                            processElement(el);
+                        });
+                        processElement(element);
+                    }
+                    //layout();
+                    scope.$watch(_.debounce(layout, 1000, { trailing: true }));
+                    //$($window).on('resize', layout);
                 }
             };
         }]);
