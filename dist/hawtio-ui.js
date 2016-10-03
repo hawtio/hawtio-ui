b529f4f49b5af7bd944d544ed9d006bdeca4b4f6
circleci <circleci@mail.com>
2016-10-03 09:51:51 +0000

    [ci skip] Update site

--- dist/hawtio-ui.js
+++ dist/hawtio-ui.js
@@ -2573,6 +2573,48 @@ var UI;
     UI._module.filter('hawtioGroupBy', UI.groupBy);
 })(UI || (UI = {}));
 
+var UI;
+(function (UI) {
+    UI._module.directive('httpSrc', ['$http', function ($http) {
+            return {
+                // do not share scope with sibling img tags and parent
+                // (prevent show same images on img tag)
+                scope: {
+                    httpSrcChanged: '='
+                },
+                link: function ($scope, elem, attrs) {
+                    function revokeObjectURL() {
+                        if ($scope.objectURL) {
+                            URL.revokeObjectURL($scope.objectURL);
+                        }
+                    }
+                    $scope.$watch('objectURL', function (objectURL, oldURL) {
+                        if (objectURL !== oldURL) {
+                            elem.attr('src', objectURL);
+                            if (typeof $scope.httpSrcChanged !== 'undefined') {
+                                $scope.httpSrcChanged = objectURL;
+                            }
+                        }
+                    });
+                    $scope.$on('$destroy', revokeObjectURL);
+                    attrs.$observe('httpSrc', function (url) {
+                        revokeObjectURL();
+                        if (url && url.indexOf('data:') === 0) {
+                            $scope.objectURL = url;
+                        }
+                        else if (url) {
+                            $http.get(url, { responseType: 'arraybuffer' })
+                                .then(function (response) {
+                                var blob = new Blob([response.data], { type: attrs['mediaType'] ? attrs['mediaType'] : 'application/octet-stream' });
+                                $scope.objectURL = URL.createObjectURL(blob);
+                            });
+                        }
+                    });
+                }
+            };
+        }]);
+})(UI || (UI = {}));
+
 /**
  * @module UI
  */
