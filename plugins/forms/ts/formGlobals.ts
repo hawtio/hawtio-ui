2012582d61ff95bff94ef7b4af9dbfe00f80fabd
Alexandre Kieling <alex.kieling@gmail.com>
2019-02-22 11:16:10 -0300

    move hawtio-forms into hawtio-ui

---
+++ plugins/forms/ts/formGlobals.ts
@@ -0,0 +1,8 @@
+namespace Forms {
+
+  export const pluginName = 'hawtio-forms';
+  export const templateUrl = 'plugins/forms/html/';
+  export const log: Logging.Logger = Logger.get(pluginName);
+
+}
+
